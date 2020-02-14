bf5ba3607e109659a3b8f78afae8e3b59ff9b019
Pranav Singh <pranav.horror@gmail.com>
2020-02-14 13:30:25 +0530

    Added cookie file

---
+++ cookies/set.php
@@ -0,0 +1,9 @@
+<?php
+
+// it takes 6 parameters.
+// any amount of time can be given.
+// below is name,value,time of life
+
+setcookie("zeus","lorem ipsum",time()+1*60);
+
+?>
---
+++ questions/bill.php
@@ -0,0 +1,12 @@
+
+<!-- calculate bill: input units as int or float only -->
+<!-- first 30  : rs 3.52 per unit -->
+<!-- next 50   : rs 4.30 per unit -->
+<!-- next 50   : rs 5.20 per unit -->
+<!-- above 130 : rs 6.50 per unit -->
+
+<?php
+
+
+
+?>
